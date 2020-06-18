c12e210e427b53f4c9287be06945fe84363f0ffb
daninavagar <daninavagar@gmail.com>
2020-06-18 12:23:20 +0200

    OK

--- Programas_para_Txema/MegaPrograma.cpp
+++ Programas_para_Txema/MegaPrograma.cpp
@@ -19,7 +19,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <math.h>
 #include <string.h>
 
@@ -261,6 +260,8 @@ void OrdenarPalabras() {
 
             }
     }
+    
+
         
     printf("\nMuestra el array ordenado\n");
 
